8c8bd4b9b5a02ae5ccaba26c0294feb231c79bcf
Quinton Fults <coding.banjo@gmail.com>
2020-05-27 14:27:00 -0700

    code

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express')
 
 const app = express()
 
+app.engine('.hbs', require('express-handlebars')({ extname: '.hbs' }))
 app.use(express.urlencoded({ extended: true }))
+app.set('view engine', '.hbs')
 app.use(express.json())
 
 app.use(require('./routes'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
 
 app.listen(3000, () => console.log('http://localhost:3000'))
